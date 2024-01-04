88bde3fdc3e9867d1e9d22e6a4604ddf6b4f0089
Tatiana <tatiana.ch35@gmail.com>
2024-01-04 16:20:17 -0500

    4th commit

--- src/test/java/co/wedevx/digitalbank/automation/ui/utilities/Driver.java
+++ src/test/java/co/wedevx/digitalbank/automation/ui/utilities/Driver.java
@@ -25,55 +25,60 @@ public class Driver {
     }
     public static WebDriver getDriver(){
 
-        String jenkinsHome = System.getenv("JENKINS_HOME");
 
-        if (jenkinsHome != null && !jenkinsHome.isEmpty()) {
-
-            WebDriverManager.chromedriver().setup();
-            ChromeOptions options1 = new ChromeOptions();
-            options1.addArguments("--window-size=1920,1080");
-            options1.addArguments("--disable-extensions");
-            options1.setExperimentalOption("useAutomationExtension", false);
-            options1.addArguments("--proxy-sever='direct://'");
-            options1.addArguments("--proxy-bypass-list=*");
-            options1.addArguments("--start-maximized");
-            options1.addArguments("--headless");
-            driver = new ChromeDriver(options1);
-
-        } else if
-        (driver == null){
+        if(driver == null) {
             String browser = ConfigReader.getPropertiesValeu("digitalbank.browser");
-        switch(browser.toLowerCase()){
-            case "chrome":
-                System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\NewDriver\\chromedriver-win64\\chromedriver.exe");
+            String jenkinsHome = System.getenv("JENKINS_HOME");
+
+            if (jenkinsHome != null && !jenkinsHome.isEmpty()) {
+
+                System.out.println("Running on Jenkins");
+
+                WebDriverManager.chromedriver().setup();
                 ChromeOptions options1 = new ChromeOptions();
+                options1.addArguments("--window-size=1920,1080");
+                options1.addArguments("--disable-extensions");
+                options1.setExperimentalOption("useAutomationExtension", false);
+                options1.addArguments("--proxy-sever='direct://'");
+                options1.addArguments("--proxy-bypass-list=*");
+                options1.addArguments("--start-maximized");
+                options1.addArguments("--headless");
                 driver = new ChromeDriver(options1);
-                break;
-            case "safari":
-                driver = new SafariDriver();
-                break;
-            case "ie":
-                WebDriverManager.iedriver().setup();
-                driver = new InternetExplorerDriver();
-                break;
-            case "headless":
-                System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\NewDriver\\chromedriver-win64\\chromedriver.exe");
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--window-size=1920,1080");
-                options.addArguments("disable-extensions");
-                options.setExperimentalOption("useAutomationExtension", false);
-                options.addArguments("--proxy-server='direct://'");
-                options.addArguments("--proxy-bypass-list=*");
-                options.addArguments("--start-maximized");
-                options.addArguments("--headless");
-                driver = new ChromeDriver(options);
-                break;
-            case "firefox":
-            default:
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-                break;
-        }
+
+
+            } else {
+                switch (browser.toLowerCase()) {
+                    case "chrome":
+                        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\NewDriver\\chromedriver-win64\\chromedriver.exe");
+                        ChromeOptions options1 = new ChromeOptions();
+                        driver = new ChromeDriver(options1);
+                        break;
+                    case "safari":
+                        driver = new SafariDriver();
+                        break;
+                    case "ie":
+                        WebDriverManager.iedriver().setup();
+                        driver = new InternetExplorerDriver();
+                        break;
+                    case "headless":
+                        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\NewDriver\\chromedriver-win64\\chromedriver.exe");
+                        ChromeOptions options = new ChromeOptions();
+                        options.addArguments("--window-size=1920,1080");
+                        options.addArguments("disable-extensions");
+                        options.setExperimentalOption("useAutomationExtension", false);
+                        options.addArguments("--proxy-server='direct://'");
+                        options.addArguments("--proxy-bypass-list=*");
+                        options.addArguments("--start-maximized");
+                        options.addArguments("--headless");
+                        driver = new ChromeDriver(options);
+                        break;
+                    case "firefox":
+                    default:
+                        WebDriverManager.firefoxdriver().setup();
+                        driver = new FirefoxDriver();
+                        break;
+                }
+            }
         }
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
